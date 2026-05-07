2de3ef0d559072d5fa390883d4d4666c4fa12dd5
agthomas-uc <agthomas@uchicago.edu>
2026-05-07 10:23:37 -0500

    I rewrote lines 5, 6, 12, 13, 14, 15, and 16 to be consistent with the changes I made to the put_in_row.ipynb file. These changes were my best attempt to put the ducks in a row. As you can see, the second duck did NOT turn properly. I think this is because Python itself is very buggy and unreliable. The third duck is properly centered. IDK what happened to the first duck but maybe he deserved it.

--- plot.py
+++ plot.py
@@ -3,15 +3,20 @@ import matplotlib.image as mpimg
 import numpy as np
 from scipy import *
 
+from scipy.ndimage import rotate
+
 img1 = mpimg.imread('image1.png')
 image2 = mpimg.imread('image2.png')
 im3 = mpimg.imread('image3.png')
 img4 = mpimg.imread('image4.png')
-fig,ax= plt.subplots(1,4)
-ax[0].imshow(img1)
-ax[1].imshow(image2)
-ax[2].imshow(im3)
-ax[3].imshow(img4)
 
-plt.axis('off') 
-plt.show()
+rotated2 = rotate(image2, -15, reshape=False)
+shifted3 = np.roll(im3, 40, axis=0)
+
+### This is all self-explanatory
+fig,ax= plt.subplots(1,4)
+ax[0].imshow(img1[::-1])
+ax[1].imshow(rotated2*np.cos(np.deg2rad(15))) # Rotates the duck back???
+ax[2].imshow(np.roll(im3, 60, axis=0)) # Shift the duck down by 60 pixels to center it better
+ax[3].imshow(img4)
+plt.axis('off')
